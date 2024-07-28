5be3c6fd82d0f0ad62e9f35daf7fbdc064c8cbd9
iotran207 <lthlvn2007@gmail.com>
2024-07-28 10:50:40 +0700

    remove deadcode v2

--- judge/models/profile.py
+++ judge/models/profile.py
@@ -131,7 +131,7 @@ class Badge(models.Model):
     def __str__(self):
         return self.name
 
-def user_directory_path(instance, filename):
+def user_directory_path(instance):
     return os.path.join('avatar', f'{instance.id}.png')
 
 class Profile(models.Model):
