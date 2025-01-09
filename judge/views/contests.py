721501911e657abd0f659ed921a8af3bae0769fe
iotran207 <lthlht2007@gmail.com>
2025-01-09 08:08:48 +0700

    add allow_seb field to Contest model and implement SEB validation in ContestJoin view

--- judge/views/contests.py
+++ judge/views/contests.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import os
 from calendar import Calendar, SUNDAY
@@ -563,6 +564,18 @@ class ContestJoin(LoginRequiredMixin, ContestMixin, SingleObjectMixin, View):
                                    _('"%s" is not currently ongoing.') % contest.name)
 
         profile = request.profile
+        
+        if contest.allow_seb:
+            CONTEST_ID = contest.id
+            CONTEST_ID_HASH = hashlib.md5(str(CONTEST_ID).encode('utf-8')).hexdigest()
+            USER_AGENT = request.META['HTTP_USER_AGENT'].split(';')[-1]
+
+            print("id hash",CONTEST_ID_HASH)
+            print('agent',USER_AGENT)
+            
+            if USER_AGENT != CONTEST_ID_HASH:
+                return generic_message(request, _('Không phát hiện SEB đang hoạt động'),
+                           _('Yêu cầu thí sinh khởi động phần mềm SEB để bắt đầu vào thi'))
 
         if not request.user.is_superuser and contest.banned_users.filter(id=profile.id).exists():
             return generic_message(request, _('Banned from joining'),
