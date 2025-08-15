98401f722a81373c5f74b47976ddc6a6a1f32807
longaluminium10 <longaluminium10@gmail.com>
2025-08-15 10:22:20 +0000

    fck flake check 🤬btw no auto formating code?

--- judge/user_translations.py
+++ judge/user_translations.py
@@ -7,7 +7,7 @@ if settings.USE_I18N:
     _translations = {}
 
     def translation(language):
-        global _translations
+        # global _translations
         if language not in _translations:
             _translations[language] = DjangoTranslation(language, domain='dmoj-user')
         return _translations[language]
